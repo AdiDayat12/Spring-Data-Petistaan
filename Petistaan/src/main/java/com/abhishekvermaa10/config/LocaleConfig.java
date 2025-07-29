5428da888e6c26a99ffb09573269e897cb21208e
Nseva <adilinan72@gmail.com>
2025-07-29 11:23:53 +0300

    Add configuration classess

---
+++ Petistaan/src/main/java/com/abhishekvermaa10/config/LocaleConfig.java
@@ -0,0 +1,17 @@
+package com.abhishekvermaa10.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class LocaleConfig {
+    @Bean
+    MessageSource messageSource () {
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setBasename("classpath:messages");
+        source.setDefaultEncoding("UTF-8");
+        return source;
+    }
+}
